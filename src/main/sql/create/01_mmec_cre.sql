2d8871435148345cf390963f0b8a4e480205a0eb
Samuel Dussault <samuel_dusso@hotmail.com>
2022-09-02 16:48:10 -0400

    First version of mmec table

--- src/main/sql/create/01_mmec_cre.sql
+++ src/main/sql/create/01_mmec_cre.sql
@@ -1,1 +1,8 @@
-create table mmec.test(pk int primary key);
+create table mmec.mmec
+(
+  mmec_id int,
+  ontorel_ref_id text,
+  source_ref_id text,
+  last_modified timestamptz,
+  primary key (mmec_id)
+);
