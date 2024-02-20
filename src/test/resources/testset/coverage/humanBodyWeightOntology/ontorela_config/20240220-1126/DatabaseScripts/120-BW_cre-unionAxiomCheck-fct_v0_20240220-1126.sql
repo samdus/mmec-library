57ad7c4d2c6d6a5fe2a90a092c40667232a6929e
Samuel Dussault <samuel_dusso@hotmail.com>
2024-02-20 17:17:50 -0500

    Nouvel OntoRel pour l'ontologie coverage

---
+++ src/test/resources/testset/coverage/humanBodyWeightOntology/ontorela_config/20240220-1126/DatabaseScripts/120-BW_cre-unionAxiomCheck-fct_v0_20240220-1126.sql
@@ -0,0 +1,14 @@
+/*
+-- =========================================================================== A
+Schema : BW
+Creation Date : 20240220-1126
+Encoding : UTF-8, sans BOM, fin de ligne Unix (LF)
+Plateforme : PostgreSQL 9.6
+Responsable : OntoRelA
+Version : v0
+Status : dev
+Objet :
+  Create check union axiom functions
+-- =========================================================================== A
+*/
+
