5c129a2f3d59f8ed226d11b100d35856c71e59e2
Samuel Dussault <samuel_dusso@hotmail.com>
2024-02-21 14:49:31 -0500

    Découpler la gestion de la fonction RDF à celle de IRIStringTemplate

--- src/main/java/ca/griis/mmec/controller/ontop/iq/transform/IndividuationFunctionQueryTransformer.java
+++ src/main/java/ca/griis/mmec/controller/ontop/iq/transform/IndividuationFunctionQueryTransformer.java
@@ -24,6 +24,7 @@ import it.unibz.inf.ontop.model.term.RDFTermTypeConstant;
 import it.unibz.inf.ontop.model.term.TermFactory;
 import it.unibz.inf.ontop.model.term.Variable;
 import it.unibz.inf.ontop.model.term.functionsymbol.RDFTermFunctionSymbol;
+import it.unibz.inf.ontop.model.term.functionsymbol.db.IRIStringTemplateFunctionSymbol;
 import it.unibz.inf.ontop.model.type.TermType;
 import it.unibz.inf.ontop.model.type.impl.IRITermType;
 import it.unibz.inf.ontop.substitution.Substitution;
@@ -71,7 +72,7 @@ public class IndividuationFunctionQueryTransformer extends
     Substitution<ImmutableTerm> substitution = new SubstitutionImpl<>(
         ImmutableMap.copyOf(constructionNode.getSubstitution().stream()
             .collect(Collectors.toMap(Map.Entry::getKey,
-                value -> getTermOrChangeRdfFunctionForIndividuationFunction(tree, value)))),
+                value -> updateTermIfRelatedToIndividuation(tree, value)))),
         termFactory, true);
 
     ConstructionNode newConstructionNode = iqFactory.createConstructionNode(
@@ -80,36 +81,52 @@ public class IndividuationFunctionQueryTransformer extends
     return transformUnaryNode(tree, newConstructionNode, child);
   }
 
-  private ImmutableTerm getTermOrChangeRdfFunctionForIndividuationFunction(IQTree tree,
+  /**
+   * @brief @~english «Description of the method»
+   * @param tree «Parameter description»
+   * @param substitutionEntry «Parameter description»
+   * @return «Return description»
+   *
+   * @brief @~french Updates the term if it is related to individuation.
+   * @param tree The IQTree in which the substituted term is defined.
+   * @param substitutionEntry The substitution entry to update.
+   * @return The value of the substitution entry if not related to individuation.
+   *         The inner term if the substitution was a call to the RDF function, without
+   *         any call to the IRIStringTemplateFunction.
+   *         A new term MMEcSignatureFunction term if the substitution was a call to the
+   *         IRIStringTemplateFunction or a call to RDF with an inner term that was a
+   *         call to the IRIStringTemplateFunction.
+   *
+   * @par Tâches
+   *    S.O.
+   */
+  private ImmutableTerm updateTermIfRelatedToIndividuation(IQTree tree,
       Map.Entry<Variable, ImmutableTerm> substitutionEntry) {
+    ImmutableTerm newTerm = substitutionEntry.getValue();
+
     if (substitutionEntry.getValue() instanceof NonGroundFunctionalTerm term
         && term.getFunctionSymbol() instanceof RDFTermFunctionSymbol
         && term.getTerm(1) instanceof RDFTermTypeConstant rdfTermTypeConstant
         && rdfTermTypeConstant.getRDFTermType() instanceof IRITermType) {
-
-      if (term.getTerm(0) instanceof NonGroundFunctionalTerm identityRdfFunction) {
-        DBConstant identifierForSignatureGroup = termFactory.getDBStringConstant(
-            identityRdfFunction.getFunctionSymbol().getName());
-        ImmutableList<ImmutableTerm> arguments = ImmutableList.<ImmutableTerm>builder()
-            .add(identifierForSignatureGroup)
-            .addAll(term.getVariables().asList())
-            .build();
-        ImmutableList<TermType> argTypes =
-            arguments.stream().map(
-                variable -> typeExtractor.extractSingleTermType(variable, tree).orElseThrow())
-                .collect(ImmutableList.toImmutableList());
-
-        return termFactory.getMMecSignatureFunction(argTypes, arguments);
-      } else {
-        // TODO: Vérifier pourquoi est-ce qu'un exception est lancée ici lorsqu'on a une définition
-        //       d'union au premier niveau.
-        //       ex: Classe http://www.griis.ca/projects/tst
-        throw new UnsupportedOperationException(
-            "The first argument of the RDF term function must be a functional term. It is used by"
-                + " Ontop to generate the IRI and used in MMec as the identifier for the signature"
-                + " group.");
-      }
+      newTerm = term.getTerm(0);
     }
-    return substitutionEntry.getValue();
+
+    if (newTerm instanceof NonGroundFunctionalTerm term &&
+        term.getFunctionSymbol() instanceof IRIStringTemplateFunctionSymbol iriStrTemplateFn) {
+      DBConstant identifierForSignatureGroup = termFactory.getDBStringConstant(
+          iriStrTemplateFn.getName());
+      ImmutableList<ImmutableTerm> arguments = ImmutableList.<ImmutableTerm>builder()
+          .add(identifierForSignatureGroup)
+          .addAll(term.getVariables().asList())
+          .build();
+      ImmutableList<TermType> argTypes =
+          arguments.stream().map(
+                  variable -> typeExtractor.extractSingleTermType(variable, tree).orElseThrow())
+              .collect(ImmutableList.toImmutableList());
+
+      return termFactory.getMMecSignatureFunction(argTypes, arguments);
+    }
+
+    return newTerm;
   }
 }
