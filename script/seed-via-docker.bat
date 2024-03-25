5ce7e89db5c61e887fa7ad4c7f9b0994b86c2e35
Samuel Dussault <samuel_dusso@hotmail.com>
2024-03-25 13:53:08 -0400

    Revert "initialiser ontologie via odk"
    This reverts commit bd8e3a92fdbd02b625568f0a8d87067d11c66840.

--- script/seed-via-docker.bat
+++
@@ -1,2 +0,0 @@
-::BATCH file to seed new ontology repo via docker
-docker run -v %userprofile%/.gitconfig:/root/.gitconfig -v %cd%:/work -w /work --rm -ti obolibrary/odkfull /tools/odk.py seed %*
--- src/main/ontology/mmec/ontology/mmec.Makefile
+++
@@ -1,7 +0,0 @@
-## Customize Makefile settings for mmec
-## 
-## If you need to customize your Makefile, make
-## changes here rather than in the main Makefile
-
-
-
