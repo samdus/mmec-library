6414b349bd16c7f526bf86005b310d6b276fee9e
Samuel Dussault <samuel_dusso@hotmail.com>
2024-04-12 13:02:17 -0400

    Coercition de type obligatoire dans mapping sans mmec

--- src/test/resources/testset/coverage/database.sql
+++ src/test/resources/testset/coverage/database.sql
@@ -4,4 +4,5 @@
 \i '/docker-entrypoint-initdb.d/coverage/jedis-griis/03a-EXP_coe.sql'
 \i '/docker-entrypoint-initdb.d/coverage/jedis-griis/05A-EXP_MAJForCoverage.sql'
 
+\i '/docker-entrypoint-initdb.d/coverage/humanBodyWeightOntology/ontorela_config/20240313-1437/DatabaseScripts/000-BW_cre-domain_v0_20240313-1437.sql'
 \i '/docker-entrypoint-initdb.d/coverage/humanBodyWeightOntology/ontorela_config/20240313-1437/DatabaseScripts/1003-BW_OntoRelCat_ins_v0_20240313-1437.sql'
--- src/test/resources/testset/coverage/humanBodyWeightOntology/ontorela_config/20240313-1437/DatabaseScripts/100-BW_cre-table_v0_20240313-1437.sql
+++ src/test/resources/testset/coverage/humanBodyWeightOntology/ontorela_config/20240313-1437/DatabaseScripts/100-BW_cre-table_v0_20240313-1437.sql
@@ -14,32 +14,6 @@ Objet :
 
 CREATE SCHEMA IF NOT EXISTS "BW";
 
-COMMENT ON SCHEMA "BW" IS 'Schéma BW créé le 20240313-1437';
-
--- uid_domain domain definition
-CREATE DOMAIN "BW"."uid_domain" AS TEXT;
-
--- value_domain domain definition
-CREATE DOMAIN "BW"."value_domain" AS TEXT;
-
--- Literal domain definition
-CREATE DOMAIN "BW"."Literal" AS TEXT;
-
--- boolean domain definition
-CREATE DOMAIN "BW"."boolean" AS BOOLEAN;
-
--- langString domain definition
-CREATE DOMAIN "BW"."langString" AS TEXT;
-
--- string domain definition
-CREATE DOMAIN "BW"."string" AS TEXT;
-
--- double domain definition
-CREATE DOMAIN "BW"."double" AS DOUBLE PRECISION;
-
--- decimal domain definition
-CREATE DOMAIN "BW"."decimal" AS DECIMAL;
-
 -- table Thing definition
 CREATE TABLE "BW"."Thing"
 (
