a90cfe05b8995b6485386aaf6e60efed097b310d
Samuel Dussault <samuel_dusso@hotmail.com>
2024-12-09 09:38:01 -0500

    Revert "Remove an unused class"
    This reverts commit 1f1bbf635ce5fe1d61a15538409bb4ad5906e697.

---
+++ src/main/java/ca/griis/mmec/controller/ontop/spec/mapping/transformer/impl/MMecTermTypeMappingCaster.java
@@ -0,0 +1,52 @@
+/**
+ * @file
+ *
+ * @copyright @@GRIIS_COPYRIGHT@@
+ *
+ * @licence @@GRIIS_LICENCE@@
+ *
+ * @version @@GRIIS_VERSION@@
+ *
+ * @brief @~french Implémentation de la classe MMecTermTypeMappingCaster.
+ * @brief @~english MMecTermTypeMappingCaster class implementation.
+ */
+
+package ca.griis.mmec.controller.ontop.spec.mapping.transformer.impl;
+
+import it.unibz.inf.ontop.spec.mapping.MappingAssertion;
+import it.unibz.inf.ontop.spec.mapping.transformer.MappingCaster;
+
+/**
+ * @brief @~english «Brief component description (class, interface, ...)»
+ * @par Details
+ *      «Detailed description of the component (optional)»
+ * @par Model
+ *      «Model (Abstract, automation, etc.) (optional)»
+ * @par Conception
+ *      «Conception description (criteria and constraints) (optional)»
+ * @par Limits
+ *      «Limits description (optional)»
+ *
+ * @brief @~french Copie de it.unibz.inf.ontop.spec.mapping.transformer.impl.TermTypeMappingCaster
+ *        où on ne vérifie pas que les termes sont RDF.
+ * @par Détails
+ *      S.O.
+ * @par Modèle
+ *      S.O.
+ * @par Conception
+ *      S.O.
+ * @par Limites
+ *      S.O.
+ *
+ * @par Historique
+ *      2024-02-28 [SD] - Implémentation initiale<br>
+ *
+ * @par Tâches
+ *      S.O.
+ */
+public class MMecTermTypeMappingCaster implements MappingCaster {
+  @Override
+  public MappingAssertion transform(MappingAssertion assertion) {
+    return assertion;
+  }
+}
